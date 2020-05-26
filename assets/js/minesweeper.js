ec7a89cc67d8e4d01f9680291d9b49464f466798
Developer Hope <me@devhope.co>
2020-05-26 10:08:48 +0000

    Adding bombs and clear blocks

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".ms-grid");
   let width = 10;
+  let bombers = 20;
   let squares = [];
 
   // Creating the board
 
   function createTheBoard() {
+    // Putting the random bombs inside the grid
+    const bombsLocation = Array(bombers).fill("kaboom");
+    const clearLocation = Array(width * width - bombers).fill("clear");
+    console.log(bombsLocation);
+    console.log(clearLocation);
+
     let i;
     for (i = 0; i < width * width; i++) {
       const square = document.createElement("div");
